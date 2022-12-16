84f395607bf3307d0ea97ea9ef640911a67ca1c4
Dmitry Borisenko <borisenko.production@gmail.com>
2022-12-16 22:04:29 +0300

    Maked migrations

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+context = {
+        'index_text': 'Это главная страница проекта Yatube',
+        'groups_text': 'Здесь будет информация о группах проекта Yatube',
+    }
+
 
 def index(request):
-    return render(request, 'posts/index.html')
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа {slug}')
+    return render(request, 'posts/group_list.html', context)
