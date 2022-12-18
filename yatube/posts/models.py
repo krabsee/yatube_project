a979529f08226e97dca7aebfef54fa03ccabfef7
Dmitry Borisenko <borisenko.production@gmail.com>
2022-12-18 16:13:05 +0300

    Update after review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,25 +5,32 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Сообщество')
+    slug = models.SlugField(unique=True, verbose_name='Запись в URL')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return f'{self.title}'
 
 
 class Post(models.Model):
-    text = models.TextField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=200, verbose_name='Название поста')
+    pub_date = models.DateTimeField(auto_now_add=True,
+                                    verbose_name='Дата публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор',
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL,
+        verbose_name='Сообщество',
+        related_name='posts',
     )
+
+    class Meta:
+        ordering = ['-pub_date', ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.conf import settings
+
+posts_on_page = settings.POSTS_ON_PAGE
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related('group').all()[:posts_on_page]
     context = {
-        'index_text': 'Это главная страница проекта Yatube',
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
@@ -13,10 +15,9 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:posts_on_page]
     context = {
         'group': group,
         'posts': posts,
-        'title': group.title,
     }
     return render(request, 'posts/group_list.html', context)
